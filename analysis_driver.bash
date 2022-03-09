784ee009b1a114d1d151cb715672eb88ec9a74f5
Brian Matherne <bwmatherne@icloud.com>
2022-03-08 23:15:15 -0600

    Corrected order of operations

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -8,25 +8,25 @@
 ############################################################################
 
 # 1. Demultiplex the raw data files from Illumina run 4 at  IEG
-qsub code/seq.pbs
+qsub code/demux.pbs
 #########################################################
-#2 Combine the three output tables
+# 2. Train sequence classifier
+  # Check if the most release of the Silva database needs to be downloaded
+  # trun-lens may need to be ammended depending on type of sequences
+  # This step is memory instensive and may need to be run on nodes with more RAM
+  # Calculate reads-per batch by dividing n sequences by n processors
 
-qsub code/combine_table.pbs
-
-
-# Archive all sequencing files and move to the data/process/ folder
-tar -zcvf data/process/seq1.tar.gz data/qiime/seq1/
-rm -rf data/qiime/seq1/
-tar -zcvf data/process/seq2.tar.gz data/qiime/seq2/
-rm -rf data/qiime/seq2/
-tar -zcvf data/process/seq3.tar.gz data/qiime/seq3/
-rm -rf data/qiime/seq3/
-tar -zcvf data/process/seq4.tar.gz data/qiime/seq4/
-rm -rf data/qiime/seq4/
+qsub code/trainset.pbs
 
 ########################################################
-#3 Alpha and beta diversity analysis
+# 3. Taxonomic Analysis
+  # Create phylogenetic trees
+qsub code/phytree.pbs
+  # Generate taxa bar plot file and collapse taxa
+qsub code/taxa.pbs
+
+########################################################
+# 4. Alpha and beta diversity analysis
 	# Addapted from this tutorial https://docs.qiime2.org/2019.7/tutorials/moving-pictures/
 ### * First choose sampling depth by viewing the information presented in the table.qzv file that was created above.
 ### * Choose a value that is as high as possible (so you retain more sequences per sample) while excluding as few samples as possible.
@@ -35,32 +35,10 @@ rm -rf data/qiime/seq4/
 qsub code/diversity.pbs
 
 ########################################################
-#4 Taxonomic Analysis
-	# Check if the most release of the Silva database needs to be downloaded
-	# trun-lens may need to be ammended depending on type of sequences
-	# This step is memory instensive and may need to be run on nodes with more RAM
-	# Calculate reads-per batch by dividing n sequences by n processors
-
-qsub code/trainset.pbs
-
-
-########################################################
-#5 Export BIOM File for R stats
-
+# 5. Download and analyze your data in R
+  # Export BIOM File for R stats
 bash code/export.bash
-
-
-########################################################
-#6 Collapse Taxonomic Data
-
-qsub code/taxa.pbs
-
-########################################################
-#7 Convert Biom file to tsv
-
+  # Convert Biom file to tsv
 bash code/biom_convert.bash
-
 ########################################################
-#8 Download and analyze your data in R
-
 exit 0
